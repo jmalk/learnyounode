9decf6dea199e836bcbf8cd9dd41c5550909e71d
Josh Malkinson <josh.malkinson@gmail.com>
2016-03-04 20:12:42 +0000

    Adjust spacing

--- 09_JugglingAsync.js
+++ 09_JugglingAsync.js
@@ -1,21 +1,24 @@
 var http = require('http'),
     bl = require('bl');
 
-var urls = [process.argv[2], process.argv[3], process.argv[4]];
-
-var responses = [];
+var urls = [process.argv[2], process.argv[3], process.argv[4]],
+    responses = [];
 
 urls.forEach(httpGet);
 
 function httpGet (url, index) {
     'use strict';
+
     http.get(url, function(response) {
         response.setEncoding('utf8');
+
         response.pipe(bl(function (error, data) {
             if (error) {
                 return console.error(error);
             }
+
             responses[index] = data.toString();
+
             if (allResponsesReceived()) {
                 printAllResponses();
             }
