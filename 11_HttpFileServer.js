6829fc65033575691da24a50b4767fa06133c521
Josh Malkinson <josh.malkinson@gmail.com>
2016-03-11 14:31:05 +0000

    Cast port argument to number

--- 11_HttpFileServer.js
+++ 11_HttpFileServer.js
@@ -1,7 +1,7 @@
 var http = require('http'),
     fs = require('fs');
 
-var port = process.argv[2],
+var port = Number(process.argv[2]),
     file = process.argv[3];
 
 var server = http.createServer(function (req, res) {
