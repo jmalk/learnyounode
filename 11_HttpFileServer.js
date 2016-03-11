b7bf7e374ca662e2ca0183686bcb48f70ef2b73c
Josh Malkinson <josh.malkinson@gmail.com>
2016-03-11 14:38:38 +0000

    Add missing ".js" to default argument

--- 11_HttpFileServer.js
+++ 11_HttpFileServer.js
@@ -2,7 +2,7 @@ var http = require('http'),
     fs = require('fs');
 
 var port = Number(process.argv[2]) || 8000,
-    file = process.argv[3] || "11_HttpFileServer";
+    file = process.argv[3] || "11_HttpFileServer.js";
 
 var server = http.createServer(function (req, res) {
     'use strict';
