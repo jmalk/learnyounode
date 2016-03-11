2537f49bde8b0cb37bb8d93c17aa7c1637eda8de
Josh Malkinson <josh.malkinson@gmail.com>
2016-03-11 14:31:15 +0000

    Use strict

--- 11_HttpFileServer.js
+++ 11_HttpFileServer.js
@@ -5,6 +5,7 @@ var port = Number(process.argv[2]),
     file = process.argv[3];
 
 var server = http.createServer(function (req, res) {
+    'use strict';
     var fileStream = fs.createReadStream(file);
     fileStream.pipe(res);
 });
